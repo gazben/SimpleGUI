597fc6a973dc22c521da6d468ce3afc07047a71d
Gazben <bencegazder@gmail.com>
2014-05-01 21:19:30 +0200

    SimpleGUI.h: New functions declarations added. This class will be the interface between the user and the inner components.

--- SimpleGUI/SimpleGUI.h
+++ SimpleGUI/SimpleGUI.h
@@ -1,5 +1,34 @@
-namespace SG{
+#ifndef SimpleGUI_h__
+#define SimpleGUI_h__
+
+#include <string>
+
+#include "Mouse.h"
+#include "Displayable.h"
+
+class SimpleGUI{
+public:
+
+	void InitfromXML(std::string path);
+
+	void setMousePosition();
+	void setKeyState();
+
+	//Add a drawable element to the gui.
+	void AddElement(Displayable);
+
+	//Handle the events from the user. Ex.: Mouse
+	void HandleEvents();
+
+	//Show the gui on the screen
+	void Show();
+
+	SimpleGUI(){}
+
+private:
+	Mouse mouse;
 
 
 
-}
+};
+#endif // SimpleGUI_h__
