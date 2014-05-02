4b8aa9940b434f42037b92deb7269494a2d9f37d
Gazben <bencegazder@gmail.com>
2014-05-02 02:16:14 +0200

    SGengine.cpp: SGengine.h function definitions moved to cpp. Initwindow method implemented. SGengine construtor implemented.

--- SimpleGUI/SGengine.cpp
+++ SimpleGUI/SGengine.cpp
@@ -2,8 +2,26 @@
 
 void SGengine::Show()
 {
-	for each (GUIelement a in GUIelements){
-		if (a.isVisible())
-			a.Show();
+	for each (GUIelement* a in GUIelements){
+		if (a->isVisible())
+			a->Show();
 	}
+}
+
+void SGengine::HandleEvents()
+{
+}
+
+void SGengine::AddElement(GUIelement* inElement)
+{
+}
+
+void SGengine::InitWindow(std::string title, int width, int height)
+{
+	glfwInit();
+
+	window = glfwCreateWindow(width, height, "szar", NULL, NULL);
+
+	glfwMakeContextCurrent(window);
+
 }
