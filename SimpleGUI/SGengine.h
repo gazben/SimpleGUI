0a8c1923a533346a73a949b6eacf4f9745d7c2c1
Gazben <bencegazder@gmail.com>
2014-05-02 02:17:10 +0200

    SGengine.h: Basic fields/functions defined

--- SimpleGUI/SGengine.h
+++ SimpleGUI/SGengine.h
@@ -3,20 +3,32 @@
 
 #include <vector>
 
-#include "Displayable.h"
+#define GLFW_DLL 
+#include <GLFW/glfw3.h>
+
+#include "GUIelement.h"
 #include "Control.h"
+#include "Mouse.h"
 
 class SGengine{
 public:
 
+	void HandleEvents();
 	void Show();
-	void AddElement(Displayable);
-	void AddElement(Control);
+
+	void AddElement(GUIelement*);
+
+	void InitWindow(std::string, int height, int width);
+
+	SGengine();
 
 private:
 
-	std::vector<Displayable*>	DisplayableElements;
-	std::vector<Control*>		ControlElements;
+	std::vector<GUIelement*>	GUIelements;
+
+	Mouse mouse;
+
+	GLFWwindow* window;
 };
 
 #endif // SGengine_h__
