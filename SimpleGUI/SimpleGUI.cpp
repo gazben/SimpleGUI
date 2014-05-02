7c7b329b545faa8e5338de28cc851c856af7d0c5
Gazben <bencegazder@gmail.com>
2014-05-02 02:19:03 +0200

    SimpleGUI.cpp: .h functions will be defined here

---
+++ SimpleGUI/SimpleGUI.cpp
@@ -0,0 +1,30 @@
+#include "SimpleGUI.h"
+
+void SimpleGUI::InitWindow(std::string title, int height /*= 800*/, int width /*= 600*/)
+{
+	engine.InitWindow(title, height, width);
+}
+
+void SimpleGUI::LoadFromXML(std::string)
+{
+}
+
+void SimpleGUI::setMousePosition()
+{
+}
+
+void SimpleGUI::setKeyState()
+{
+}
+
+void SimpleGUI::AddElement(GUIelement&)
+{
+}
+
+void SimpleGUI::HandleEvents()
+{
+}
+
+void SimpleGUI::Show()
+{
+}
