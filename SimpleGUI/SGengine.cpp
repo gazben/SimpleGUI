92291228b0798782be5dbb017b7e009a6b4ee502
Gazben <bencegazder@gmail.com>
2014-05-03 14:20:53 +0200

    SGengine.cpp: Addelement function implemented, InitWindow function wait for event FOR TESTING ONLY

--- SimpleGUI/SGengine.cpp
+++ SimpleGUI/SGengine.cpp
@@ -1,4 +1,5 @@
 #include "SGengine.h"
+#include "GUIelement.h"
 
 void SGengine::Show()
 {
@@ -8,19 +9,26 @@ void SGengine::Show()
 	}
 }
 
-void SGengine::HandleEvents()
-{
+void SGengine::HandleEvents(){
 }
 
-void SGengine::AddElement(GUIelement* inElement)
-{
+void SGengine::AddElement(GUIelement& inElement){
+	GUIelements.push_back(inElement.clone());
 }
 
 void SGengine::InitWindow(std::string title, int width, int height)
 {
-	window = glfwCreateWindow(width, height, "szar", NULL, NULL);
+	window = glfwCreateWindow(width, height, title.c_str(), NULL, NULL);
 
 	glfwMakeContextCurrent(window);
+
+	//FOR TESTING ONLY!
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
+	}
 }
 
 SGengine::SGengine()
