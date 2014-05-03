b6941e4e24027cfb05681260180c58f7ca305a97
Gazben <bencegazder@gmail.com>
2014-05-04 01:26:30 +0200

    main.cpp: default testcase added

--- SimpleGUI/main.cpp
+++ SimpleGUI/main.cpp
@@ -1,48 +1,51 @@
-#include <GL/glew.h>
+#include "SimpleGUI.h"
 
-#define GLFW_DLL 
-#include <GLFW/glfw3.h>
-
-#include <FreeImage/FreeImage.h>
-
-
-
+#include "Button.h"
 /*
-The main function is for testing purposes only! 
+The main function is for testing purposes only!
 
 The main function is used to test the SimpleGUI library.
 */
-int main(void)
+int main()
 {
-	GLFWwindow* window;
+	SimpleGUI gui;
+	gui.InitWindow("SimpleGUI testwindow");
 
-	/* Initialize the library */
-	if (!glfwInit())
-		return -1;
+	Button testButton("test", 100 , 100 , 20 , 10);
 
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-	if (!window)
-	{
-		glfwTerminate();
-		return -1;
-	}
+	gui.AddElement(testButton);
 
-	/* Make the window's context current */
-	glfwMakeContextCurrent(window);
+	gui.WaitforEvents();
+	// 	GLFWwindow* window;
+	//
+	// 	/* Initialize the library */
+	// 	if (!glfwInit())
+	// 		return -1;
+	//
+	// 	/* Create a windowed mode window and its OpenGL context */
+	// 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	// 	if (!window)
+	// 	{
+	// 		glfwTerminate();
+	// 		return -1;
+	// 	}
+	//
+	// 	/* Make the window's context current */
+	// 	glfwMakeContextCurrent(window);
+	//
+	// 	/* Loop until the user closes the window */
+	// 	while (!glfwWindowShouldClose(window))
+	// 	{
+	// 		/* Render here */
+	//
+	// 		/* Swap front and back buffers */
+	// 		glfwSwapBuffers(window);
+	//
+	// 		/* Poll for and process events */
+	// 		glfwPollEvents();
+	// 	}
+	//
+	// 	glfwTerminate();
 
-	/* Loop until the user closes the window */
-	while (!glfwWindowShouldClose(window))
-	{
-		/* Render here */
-
-		/* Swap front and back buffers */
-		glfwSwapBuffers(window);
-
-		/* Poll for and process events */
-		glfwPollEvents();
-	}
-
-	glfwTerminate();
 	return 0;
 }
