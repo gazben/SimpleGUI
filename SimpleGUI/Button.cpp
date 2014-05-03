76a958211c288978a3e563c26fb8ed10b2a4c775
Gazben <bencegazder@gmail.com>
2014-05-04 00:43:38 +0200

    Button: Now it should display a rectangle in the given position

--- SimpleGUI/Button.cpp
+++ SimpleGUI/Button.cpp
@@ -1,11 +1,50 @@
 #include "Button.h"
 
-void Button::ProcessUserEvents(GLFWwindow* )
-{
+#ifdef __gl_h_
+printf("gl.h included");		//debug
+#endif
+
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+Button::Button(std::string _text, double _x, double _y, double _width, double _height){
+
+	text = _text;
+	visible = true;
+	x = _x;
+	y = _y;
+	height = _height;
+	width = _width;
 
 }
 
+Button::Button(const Button& _inButton){
+
+	text = _inButton.text;
+	visible = _inButton.visible;
+	x = _inButton.x;
+	y = _inButton.y;
+	height = _inButton.height;
+	width = _inButton.width;
+}
+
 void Button::Show(){
+	
+	glPushMatrix();
+
+	glColor3d(0.5, 0.5, 0.5);
+
+	glBegin(GL_QUADS);
+
+	glVertex2d( x, y);
+	glVertex2d( x, y + width);
+	glVertex2d( x + height, y + width);
+	glVertex2d( x + height, y );
+
+	glEnd();
+
+	glPopMatrix();
 
 }
 
@@ -17,3 +56,11 @@ GUIelement* Button::clone() const
 {
 	return new Button(*this);
 }
+
+void Button::ProcessUserEvents(GLFWwindow*)
+{
+
+}
+
+
+
--- SimpleGUI/Button.h
+++ SimpleGUI/Button.h
@@ -1,23 +1,30 @@
 #ifndef Button_h__
 #define Button_h__
 
-#include <GLFW/glfw3.h>
+#include <string>
+
 #include "GUIelement.h"
 
 class Button :public GUIelement{
 public:
 
+	Button(std::string _text, double _x, double _y, double _width, double _height);
+
+	Button(const Button& _inButton);
+
 	virtual void Show();
 
 	bool isClicked();
 
-	virtual void ProcessUserEvents(GLFWwindow*);
-
 	virtual GUIelement* clone() const;
+
+	virtual void ProcessUserEvents(GLFWwindow*);
 
 private:
 
 	bool clicked;
+
+	std::string text;
 };
 
 #endif // Button_h__
