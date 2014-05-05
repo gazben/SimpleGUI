206ce4f0eda2323f6adbde41d5a97a3ed5fcdeb3
Gazben <bencegazder@gmail.com>
2014-05-05 18:47:15 +0200

    Mouse: mouse inverted on the y coordinate for better understanding and collision detection

--- SimpleGUI/Mouse.cpp
+++ SimpleGUI/Mouse.cpp
@@ -3,6 +3,8 @@
 #define  GLFW_DLL
 #include <GLFW/glfw3.h>
 
+#include <stdio.h>
+
 int Mouse::LeftButtonPressed = false;
 int Mouse::RightButtonPressed = false;
 
@@ -22,14 +24,16 @@ void Mouse::GetMouseButtonState(GLFWwindow* window){
 	RightButtonPressed = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT);
 }
 
-void Mouse::GetMousePosition(GLFWwindow* window){
-	glfwGetCursorPos(window, &x, &y);
-}
-
 void Mouse::SetMousePosition(GLFWwindow* window, double _x, double _y){
+
+	int Screen_height;
+	int Screen_width;
+
+	glfwGetWindowSize(window, &Screen_width, &Screen_height);
+
 	glfwSetCursorPos(window, _x, _y);
 	x = _x;
-	y = _y;
+	y = Screen_height - _y;
 }
 
 Mouse::Mouse(){
@@ -39,10 +43,17 @@ Mouse::Mouse(){
 
 void Mouse::MouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
 {
-
 }
 
-void Mouse::MousePosCallback(GLFWwindow * window, double _x, double _y)
-{
+void Mouse::MousePosCallback(GLFWwindow * window, double _x, double _y){
+
+	int Screen_height;
+	int Screen_width;
+
+	glfwGetWindowSize(window, &Screen_width, &Screen_height);
+
+	x = _x;
+	y = Screen_height - _y;
+	printf("x: %lf , y: %lf\n", x, y);
 
 }
