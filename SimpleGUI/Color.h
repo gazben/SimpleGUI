ca95907356a12d01eb58dc79756ccefb6d8aef29
Gazben <bencegazder@gmail.com>
2014-05-06 01:28:12 +0200

    Color: Color handler class added and Clols namespace for stored colors

---
+++ SimpleGUI/Color.h
@@ -0,0 +1,48 @@
+struct Color {
+	float r, g, b;
+
+	Color() {
+		r = g = b = 0;
+	}
+	Color(float r0, float g0, float b0) {
+		r = r0; g = g0; b = b0;
+	}
+	Color operator*(float a) {
+		return Color(r * a, g * a, b * a);
+	}
+	Color operator*(const Color& c) {
+		return Color(r * c.r, g * c.g, b * c.b);
+	}
+	Color operator/(const Color& c) {
+		return Color(r / c.r, g / c.g, b / c.b);
+	}
+	Color operator+(const Color& c) {
+		return Color(r + c.r, g + c.g, b + c.b);
+	}
+	Color operator-(const Color& c) {
+		return Color(r - c.r, g - c.g, b - c.b);
+	}
+	float operator[](int index){
+		switch (index){
+		case 0:
+			return r;
+		case 1:
+			return g;
+		case 2:
+			return b;
+
+		default:
+			return 0;
+		}
+	}
+};
+
+namespace Colors{
+	Color red(255.0f / 255.0f, 59.0f / 255.0, 20.0f / 255.0f);
+	Color grey(196.0f / 255.0f, 196 / 255.0f, 196.0f / 255.0f);
+	Color blue(79.0f / 255.0f, 128.0f / 255.0f, 253.0f / 255.0f);
+	Color green(71.0f / 255.0f, 201.0 / 255.0f, 68.0 / 255.0f);
+	Color purple(87.0f / 255.0f, 144.0f / 255.0f, 18.0f / 255.0f);
+	Color white(1.0f, 1.0f, 1.0f);
+	Color black(0.01f, 0.01f, 0.01f);
+}
