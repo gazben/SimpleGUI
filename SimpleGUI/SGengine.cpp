5420983eed605a4506b70a8ea510a0b7d98f18b5
Gazben <bencegazder@gmail.com>
2014-05-07 01:49:24 +0200

    SGengine: clearcolor changed to white because of clack letters

--- SimpleGUI/SGengine.cpp
+++ SimpleGUI/SGengine.cpp
@@ -12,7 +12,7 @@
 
 void SGengine::Show()
 {
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	for (unsigned int i = 0; i < GUIelements.size(); i++){
