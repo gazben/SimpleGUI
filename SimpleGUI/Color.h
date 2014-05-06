d4f7e61fd0b48a71fc35e4ea549cc00db95aafec
Gazben <bencegazder@gmail.com>
2014-05-06 04:13:17 +0200

    Color: HOTFIX Colors made static because it caused linker errors

--- SimpleGUI/Color.h
+++ SimpleGUI/Color.h
@@ -41,12 +41,12 @@ struct Color {
 };
 
 namespace Colors{
-	Color red(255.0f / 255.0f, 59.0f / 255.0, 20.0f / 255.0f);
-	Color grey(196.0f / 255.0f, 196 / 255.0f, 196.0f / 255.0f);
-	Color blue(79.0f / 255.0f, 128.0f / 255.0f, 253.0f / 255.0f);
-	Color green(71.0f / 255.0f, 201.0 / 255.0f, 68.0 / 255.0f);
-	Color purple(87.0f / 255.0f, 144.0f / 255.0f, 18.0f / 255.0f);
-	Color white(1.0f, 1.0f, 1.0f);
-	Color black(0.01f, 0.01f, 0.01f);
-}
+	static Color red(255.0f / 255.0f, 59.0f / 255.0, 20.0f / 255.0f);
+	static Color grey(196.0f / 255.0f, 196 / 255.0f, 196.0f / 255.0f);
+	static Color blue(79.0f / 255.0f, 128.0f / 255.0f, 253.0f / 255.0f);
+	static Color green(71.0f / 255.0f, 201.0 / 255.0f, 68.0 / 255.0f);
+	static Color purple(87.0f / 255.0f, 144.0f / 255.0f, 18.0f / 255.0f);
+	static Color white(1.0f, 1.0f, 1.0f);
+	static Color black(0.01f, 0.01f, 0.01f);
+};
 #endif // Color_h__
