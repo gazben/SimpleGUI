e1bdcaa932cb3897f235fec2f85d76b1d2e0b35f
Gazben <bencegazder@gmail.com>
2014-05-08 00:40:25 +0200

    Color: Fixed warnings (float vs double)

--- SimpleGUI/Color.h
+++ SimpleGUI/Color.h
@@ -41,10 +41,10 @@ struct Color {
 };
 
 namespace Colors{
-	static Color red(255.0f / 255.0f, 59.0f / 255.0, 20.0f / 255.0f);
-	static Color grey(196.0f / 255.0f, 196 / 255.0f, 196.0f / 255.0f);
+	static Color red(255.0f / 255.0f, 59.0f / 255.0f, 20.0f / 255.0f);
+	static Color grey(196.0f / 255.0f, 196.0f / 255.0f, 196.0f / 255.0f);
 	static Color blue(79.0f / 255.0f, 128.0f / 255.0f, 253.0f / 255.0f);
-	static Color green(71.0f / 255.0f, 201.0 / 255.0f, 68.0 / 255.0f);
+	static Color green(71.0f / 255.0f, 201.0f / 255.0f, 68.0f / 255.0f);
 	static Color purple(87.0f / 255.0f, 144.0f / 255.0f, 18.0f / 255.0f);
 	static Color white(1.0f, 1.0f, 1.0f);
 	static Color black(0.01f, 0.01f, 0.01f);
