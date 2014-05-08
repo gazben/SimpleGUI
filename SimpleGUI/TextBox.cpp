aca4a6543578249697bb43caed704d6caf0555bc
Gazben <bencegazder@gmail.com>
2014-05-08 07:07:05 +0200

    TextBox: Implemented and working but the withoput the character load feature you cannot use it.

--- SimpleGUI/TextBox.cpp
+++ SimpleGUI/TextBox.cpp
@@ -1,19 +1,57 @@
 #include "TextBox.h"
+#include "Color.h"
 
 
+#define GLEW_STATIC
+#include <GL/glew.h>
 
+#define GLFW_DLL
+#include <GLFW/glfw3.h>
 
 GUIelement* TextBox::clone() const
 {
+	return new TextBox(*this);
+}
+
+void TextBox::ProcessUserEvents(GLFWwindow* window){
 
 }
 
-void TextBox::ProcessUserEvents(GLFWwindow* window)
-{
+void TextBox::Show(){
+
+	glColor4f(backGround.r, backGround.g, backGround.b, backGround.a);
+	glBegin(GL_QUADS);
+	glVertex2f(x, y);
+	glVertex2f(x, y + height);
+	glVertex2f(x + width, y + height);
+	glVertex2f(x + width, y);
+	glEnd();
+
+	double textpos_x = x;
+	double textpos_y = y + height;
+
+	for (int  i = 0; i < text.size(); i++){
+		char c = text[i];
+
+		glColor4f(textColor.r, textColor.g, textColor.b, textColor.a);
+		font.DrawChar(c, textpos_x, textpos_y);
+
+		textpos_x += 20;
+	}
 
 }
 
-void TextBox::Show()
+TextBox::TextBox(double _x, double _y, double _width, double _height)
 {
+	x = _x;
+	y = _y;
+	width = _width;
+	height = _height;
 
+	text = "proba";
+
+	visible = true;
+
+	backGround = Colors::blue;
+	textColor = Colors::green;
 }
--- SimpleGUI/TextBox.h
+++ SimpleGUI/TextBox.h
@@ -2,12 +2,17 @@
 #define TextBox_h__
 
 #include "GUIelement.h"
+#include "Color.h"
+#include "Font.h"
 
 #include <string>
 
 class TextBox :public GUIelement{
 
 public:
+
+	TextBox(double _x, double _y, double _width, double _height);
+
 	virtual void Show();
 
 	virtual void ProcessUserEvents(GLFWwindow* window);
@@ -17,5 +22,12 @@ public:
 private:
 
 	std::string text;
+
+	Color backGround;
+	Color textColor;
+
+	Font font;
+
+
 };
 #endif // TextBox_h__
