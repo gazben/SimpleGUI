dbbba744544304d144282654eb1d902d8b651d3e
Gazben <bencegazder@gmail.com>
2014-05-08 07:05:30 +0200

    Character: Implemented but the image load is not working.

--- SimpleGUI/Character.cpp
+++ SimpleGUI/Character.cpp
@@ -5,9 +5,22 @@
 
 void Character::Draw(double pos_x, double pos_y)
 {
+	if (!loaded){
+		loadtexture();
+		loaded = true;
+	}
+		
 
-	glRasterPos2i(pos_x, pos_y);
-	glDrawPixels(width, height, GL_RGBA, GL_UNSIGNED_BYTE, char_image);
+	glEnable(GL_TEXTURE_2D);
+
+	glBindTexture(GL_TEXTURE_2D, tex_ID);
+	glBegin(GL_QUADS);
+	glTexCoord2f(0.0, 0.0); glVertex2f(pos_x , pos_y);
+	glTexCoord2f(0.0, 1.0); glVertex2f(pos_x, pos_y + height);
+	glTexCoord2f(1.0, 1.0); glVertex2f(pos_x + width, pos_y + height);
+	glTexCoord2f(1.0, 0.0); glVertex2f( pos_x + width, pos_y);
+	glEnd();
+	glDisable(GL_TEXTURE_2D);
 }
 
 void Character::setImage(FIBITMAP* image, int left, int top, int right, int bottom)
@@ -20,17 +33,41 @@ void Character::setImage(FIBITMAP* image, int left, int top, int right, int bott
 }
 
 Character::~Character(){
-	//TODO - WTF??? if you uncomment this it will throw a debug assertion at you
-// 	if( char_image != nullptr )
-// 		delete char_image;
 }
 
-Character::Character(char _c){
+Character::Character(char _c, double _height, double _width){
+	//hack sorry I had no time to redo the font image
+
+	//upper character -> lower case
+	if (_c <= 'Z'){
+		_c = 'a' + (_c - 'A');
+	}
+	//lower case -> upper case
+	else
+	{
+		_c = 'A' + (_c - 'a');
+	}
+
 	represented_char = _c;
 
 	char_image = nullptr;
+	height = _height;
+	width = _width;
+
+	loaded = false;
 }
 
 char Character::getChar(){
 	return represented_char;
+}
+
+void Character::loadtexture()
+{
+	glGenTextures(1, &tex_ID);
+	glBindTexture(GL_TEXTURE_2D, tex_ID);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, char_image);
 }
--- SimpleGUI/Character.h
+++ SimpleGUI/Character.h
@@ -1,12 +1,18 @@
 #ifndef Character_h__
 #define Character_h__
 
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+#define GLFW_DLL
+#include <GLFW/glfw3.h>
+
 #include <FreeImage/FreeImage.h>
 
 class Character{
 public:
 
-	Character(char _c);
+	Character(char _c , double _width , double _height);
 	~Character();
 
 	void setImage(FIBITMAP* image, int left, int top, int right, int bottom);
@@ -22,5 +28,10 @@ private:
 
 	char* char_image;
 	char represented_char;
+
+	bool loaded;
+
+	void loadtexture();
+	GLuint tex_ID;
 };
 #endif // Character_h__
